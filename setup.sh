0ced17582a7bd4e7853d156840671074e8a975c1
ScarletGuo <zguo74@wisc.edu>
2019-12-07 12:18:33 -0600

    forgot flag

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get updata
 sudo apt install python3
-pip3 install mnist_hogwild/requirements.txt
-python3 main.py ---epochs 0
-python3 main.py --num-processes $1
+pip3 install -r mnist_hogwild/requirements.txt
+python3 mnist_hogwild/main.py ---epochs 0
+python3 mmnist_hogwild/ain.py --num-processes $1
