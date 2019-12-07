693215bedb99c1ee219757bd3e5082426e551e99
ScarletGuo <zguo74@wisc.edu>
2019-12-07 12:19:34 -0600

    forgot m

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ sudo apt-get updata
 sudo apt install python3
 pip3 install -r mnist_hogwild/requirements.txt
 python3 mnist_hogwild/main.py ---epochs 0
-python3 mmnist_hogwild/ain.py --num-processes $1
+python3 mmnist_hogwild/main.py --num-processes $1
