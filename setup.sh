0c08959369d5bc7ee5c7e8176a9c24799308e80e
ScarletGuo <zguo74@wisc.edu>
2019-12-07 12:21:39 -0600

    typo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-sudo apt-get updata
-sudo apt install python3
+sudo apt-get update
+sudo apt install python3-pip
 pip3 install -r mnist_hogwild/requirements.txt
 python3 mnist_hogwild/main.py ---epochs 0
 python3 mmnist_hogwild/main.py --num-processes $1
