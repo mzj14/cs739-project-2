68a8fb8841faa0aa3c4ec28b414f3b9d5dc1cca2
Ubuntu <ubuntu@ip-172-31-23-184.ec2.internal>
2019-12-15 23:56:32 +0000

    tested amazon 8

--- run_sysbench.sh
+++ run_sysbench.sh
@@ -1,5 +1,5 @@
 for i in {1..10}
 do
-	sysbench cpu --cpu-max-prime=1000000 --validate=on --histogram=on run --threads=$1 |& tee -a outputs/$2_cpu-$3.out
+	sysbench cpu --cpu-max-prime=1000000 --validate=on --histogram=on run --threads=$1 |& tee -a outputs/$2_$1_cpu-$3.out
 	echo "$i/10"
 done
