167d3fcb66f0f42ab7fcc1d443383590c1bbacc8
Zhihan Guo <zguo74@wisc.edu>
2019-12-15 11:05:26 -0600

    Update setup_sysbench.sh

--- setup_sysbench.sh
+++ setup_sysbench.sh
@@ -1,2 +1,3 @@
 curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.deb.sh | sudo bash
 sudo apt -y install sysbench
+#sysbench cpu --cpu-max-prime=1000000 --validate=on --histogram=on run
