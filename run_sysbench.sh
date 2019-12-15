afcf25fbf77dbe383994f3047c0db3c9c602860d
Ubuntu <ubuntu@ip-172-31-28-18.ec2.internal>
2019-12-15 18:44:29 +0000

    updated gpu scripts

--- run_sysbench.sh
+++ run_sysbench.sh
@@ -1,5 +1,5 @@
-for i in {1..20}
+for i in {1..10}
 do
-	sysbench cpu --cpu-max-prime=1000000 --validate=on --histogram=on run --threads=$1 |& tee -a outputs/$2_cpu.out
+	sysbench cpu --cpu-max-prime=1000000 --validate=on --histogram=on run --threads=$1 |& tee -a outputs/$2_cpu-$3.out
 	echo "$i/20"
 done
