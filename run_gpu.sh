b0c091abb586fde00c5f17f9a07043a532e3ff42
Ubuntu <ubuntu@ip-172-31-36-177.ec2.internal>
2019-12-16 02:42:01 +0000

    gpu 2

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,7 +1,7 @@
 cd mixbench/
 for i in {1..10}
 do
-	./mixbench-ocl-alt |& tee -a ../outputs/$1_gpu_alt-1.out
-	./mixbench-ocl-ro |& tee -a ../outputs/$1_gpu_ro-1.out
+	./mixbench-ocl-alt |& tee -a ../outputs/$1_gpu_alt-$2.out
+	./mixbench-ocl-ro |& tee -a ../outputs/$1_gpu_ro-$2.out
 	echo "$i/10"
 done
