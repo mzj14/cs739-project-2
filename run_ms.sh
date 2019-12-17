9e061871a9956ab130e78766f8963c3d1a096641
Ubuntu <zhihan@4cpu.2vne2kta1wbupgdxwzeox5ix4h.bx.internal.cloudapp.net>
2019-12-17 00:32:33 +0000

    microsoft 4

---
+++ run_ms.sh
@@ -0,0 +1,6 @@
+ # $1 should be provider_numbervcpus e.g. amazon_4_ml.out for amazon instance with 4 vcpus.
+for i in {1..3}
+do
+	./run_ml.sh microsoft_4 $i
+	#python3 mnist_hogwild/main.py --epochs 4 --log-interval 100 --num-processes 4 |& tee -a outputs/$1_ml-$2.out
+done
